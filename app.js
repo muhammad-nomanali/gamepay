cfee9ee3c8f63c044a6401201cedfc23911532f3
Muhammad Noman <122905854+muhammad-nomanali@users.noreply.github.com>
2025-01-30 22:44:28 -0800

    jj

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 window.addEventListener("load", function () {
     document.getElementById("loader").style.display = "none";
   });
+
+
+//   
+function copyContractAddress() {
+    const contractText = document.getElementById("contract-text").textContent;
+    navigator.clipboard.writeText(contractText).then(() => {
+        alert("Contract address copied!");
+    }).catch(err => {
+        console.error("Failed to copy: ", err);
+    });
+}
